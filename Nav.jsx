ffc87947032aef4c817c05ada501e22dd42fe4d4
abhaysheladiya <75667369+abhaysheladiya@users.noreply.github.com>
2025-02-09 11:24:31 +0530

    updated components files

--- Nav.jsx
+++ Nav.jsx
@@ -1,18 +1,28 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-
+import React, { useEffect } from 'react';
+import { Link, useNavigate} from 'react-router-dom';
 const Nav=()=>{
+    const auth= localStorage.getItem('user');
+    const logout =()=>{
+        localStorage.clear();
+        Navigate('/signup')
+    }
     return (
         <div>
-            <ul className="nav-ul">
+            <img className='logo' alt="logo" src='https://yt3.googleusercontent.com/ytc/AIdro_lpwLOOTumlQiiMYMHbBgJfQXVyRBGrZdTZ6NbtY-YA8wg=s900-c-k-c0x00ffffff-no-rj'/>
+           {auth ? <ul className="nav-ul">
                 <li><Link to="/"> Home </Link></li>
                 <li><Link to="/add">Add products</Link></li>
                 <li><Link to="/update">Update products</Link></li>
-                <li><Link to="/logout">Logout</Link></li>
                 <li><Link to="/profile">Profile</Link></li>
-                <li><Link to="/signup">SignUp</Link></li>
+                <li> <Link onClick={logout} to="/signup">Logout ({JSON.parse(auth).name}) </Link></li>
+             </ul> 
+             :
+            <ul className='nav-ul nav-right'> 
+                 <li><Link to="/signup">SignUp</Link></li>
+                 <li><Link to="/login">Login</Link></li>
             </ul>
+           }
+           
         </div>
     )
 }
