07363f1bfc8e902587da18729dd03e1355274cd0
abhaysheladiya <abhaysheladiya@gmail.com>
2025-02-09 10:24:51 +0530

    InputValidation AddProduct component

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -5,8 +5,16 @@ const AddProduct=()=>{
     const [price, setPrice] = React.useState('');
     const [category, setCategory] = React.useState('');
     const [company, setCompany] = React.useState('');
-    
+    const [error, setError] = React.useState(false)
+
     const addProduct=async()=>{
+        console.warn(!name);
+        if(!name || !price || !category || !company)
+        {
+            setError(true)
+            return false;
+        }
+
         console.warn(name,price,category,company);
         const userId =JSON.parse(localStorage.getItem('user'))._id;
         const result = await fetch("http://localhost:5000//add-product",{
@@ -25,12 +33,20 @@ const AddProduct=()=>{
             <h1>Add product</h1>
             <input type="text" placeholder='Enter Product Name' className='inputBox'
             onChange={(e)=>{setName(e.target.value)}} value={name}/>
+            {error && !name && <span className='invalid-input'>Enter Valid Name</span>}
+
             <input type="text" placeholder='Enter Product Price' className='inputBox'
             onChange={(e)=>{setPrice(e.target.value)}} value={price}/>
+             {error && !price && <span className='invalid-input'>Enter Valid Price</span>}
+
             <input type="text" placeholder='Enter Product Category' className='inputBox'
             onChange={(e)=>{setCategory(e.target.value)}} value={category}/>
+             {error && !category && <span className='invalid-input'>Enter Valid Category</span>}
+
             <input type="text" placeholder='Enter Product Company' className='inputBox'
             onChange={(e)=>{setCompany(e.target.value)}} value={company}/>
+            {error && !company && <span className='invalid-input'>Enter Valid Company</span>}
+
             <button onClick={addProduct} className='button'>Add Product</button>
         </div>
     )
