5f5fabeb4986dded60e5da5c538d36cf0650a665
abhaysheladiya <75667369+abhaysheladiya@users.noreply.github.com>
2025-02-10 21:48:39 +0530

    Integrate ProductList Component and API

--- App.jsx
+++ App.jsx
@@ -3,20 +3,29 @@ import './App.css'
 import Nav from './components/Nav'
 import Footer from './components/Footer'
 import SignUp from './components/SignUp'
+import Login from './components/Login'
+import PrivateComponent from './components/PrivateComponent'
+import ProductList from './components/ProductList'
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
-
+import AddProduct from './components/AddProduct'
 function App() { 
   return (
     <div className="App"> 
     <BrowserRouter>
        <Nav />
        <Routes>
-          <Route path="/" element={<h1> product listening component</h1>}></Route>
-          <Route path="/add" element={<h1> Add product</h1>}></Route>
-          <Route path="/update" element={<h1> update listening component</h1>}></Route>
-          <Route path="/logout" element={<h1> logout listening component</h1>}></Route>
-          <Route path="/profile" element={<h1> profile listening component</h1>}></Route>
+        <Route element={<PrivateComponent />}>
+          <Route path="/" element={<ProductList />} />
+          <Route path="/" element={<h1> product listening component</h1>} />
+          <Route path="/add" element={<AddProduct />} />
+          <Route path="/update" element={<h1> update listening component</h1>} />
+          <Route path="/logout" element={<h1> logout listening component</h1>} />
+          <Route path="/profile" element={<h1> profile listening component</h1>} />
+          </Route>
+
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+
        </Routes>
     </BrowserRouter>
     <Footer />
