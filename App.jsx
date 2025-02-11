b12e5c8fd8ab20543e1bb86f482e7e1e954489bd
abhaysheladiya <75667369+abhaysheladiya@users.noreply.github.com>
2025-02-11 10:57:02 +0530

    UpdateComponent and API

--- App.jsx
+++ App.jsx
@@ -6,6 +6,7 @@ import SignUp from './components/SignUp'
 import Login from './components/Login'
 import PrivateComponent from './components/PrivateComponent'
 import ProductList from './components/ProductList'
+import updateComponent from './components/UpdateComponent'
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import AddProduct from './components/AddProduct'
 function App() { 
@@ -18,7 +19,7 @@ function App() {
           <Route path="/" element={<ProductList />} />
           <Route path="/" element={<h1> product listening component</h1>} />
           <Route path="/add" element={<AddProduct />} />
-          <Route path="/update" element={<h1> update listening component</h1>} />
+          <Route path="/update" element={<updateComponent />} />
           <Route path="/logout" element={<h1> logout listening component</h1>} />
           <Route path="/profile" element={<h1> profile listening component</h1>} />
           </Route>
